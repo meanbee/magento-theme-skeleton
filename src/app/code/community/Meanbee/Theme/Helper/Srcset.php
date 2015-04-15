e5d8d7727692cf970b39bfa6f96d8134ba193606
Darren Belding <darren.belding@meanbee.com>
2015-04-15 12:09:05 +0100

    SKEL-9: Add theme helper to return image srcset attribute for multiple resolutions

--- src/app/code/community/Meanbee/Theme/Helper/Srcset.php
+++ src/app/code/community/Meanbee/Theme/Helper/Srcset.php
@@ -1,4 +1,19 @@
 <?php
 class Meanbee_Theme_Helper_Srcset extends Mage_Core_Helper_Data {
 
+    public function getResolutionSrcset($path, $height, $width, $value) {
+
+        $srcset = array();
+
+        for (; $value > 0; $value--) {
+            $current_width = $width * $value;
+            $current_height = $height * $value;
+
+            $resized_image = (string) $path->resize($current_width, $current_height);
+
+            $srcset[] = sprintf("%s %sx", $resized_image, $value);
+        }
+
+        return join(', ', array_reverse($srcset));
+    }
 }
