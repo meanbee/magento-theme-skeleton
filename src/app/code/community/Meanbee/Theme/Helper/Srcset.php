5f3a180938407d8332324e14c235d88ee61ff165
Darren Belding <darren.belding@meanbee.com>
2015-05-18 16:17:53 +0100

    Add docblock to image resolution srcset helper

--- src/app/code/community/Meanbee/Theme/Helper/Srcset.php
+++ src/app/code/community/Meanbee/Theme/Helper/Srcset.php
@@ -1,6 +1,17 @@
 <?php
 class Meanbee_Theme_Helper_Srcset extends Mage_Core_Helper_Data {
 
+    /**
+     * Build a HTML srcset attribute value with resized image
+     * paths up to the resolution provided as $value
+     *
+     * @param Mage_Catalog_Helper_Image $path
+     * @param int $height
+     * @param int $width
+     * @param int $value
+     *
+     * @return string
+     */
     public function getResolutionSrcset($path, $height, $width, $value) {
 
         $srcset = array();
